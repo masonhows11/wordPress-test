394494f1244022dc3d6684bd99f38be3c8abfb7f
mason.hows11 <mason.hows11@gmail.com>
2024-07-23 16:41:27 +0330

    start auth sys plugin

---
+++ wp-content/plugins/wordpress-auth/wordpress-auth.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: wordPress Apis
+Plugin URI: http://websolor.ir/
+Description: A wordPress plugin to work with apis
+Author: Naeem Soltany
+Text Domain: shortcodes test
+Version: 1.0.0
+Author URI: http://websolor.ir/
+*/
+
+define('WP_AUTH_DIR', plugin_dir_path(__FILE__));
+define('WP_AUTH_URL', plugin_dir_url(__FILE__));
+define('WP_AUTH_INC',WP_AUTH_DIR.'/inc/');
+define('WP_AUTH_TPL',WP_AUTH_DIR.'/template/');
