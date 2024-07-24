72524df1e1f58cd8be058de7846818b141b6a270
mason.hows11 <mason.hows11@gmail.com>
2024-07-24 23:16:05 +0330

    show assets codes

---
+++ wp-content/plugins/wordpress-auth/inc/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+// add styles & scripts
+
+function wp_auth_load_assets(){
+
+    // add styles
+    wp_register_style('wp_auth_style',WP_AUTH_URL . 'assets/css/auth.css');
+    wp_enqueue_style('wp_auth_style');
+
+    // add scripts
+    wp_register_script('wp_auth_scripts',WP_AUTH_URL . 'assets/css/auth.js',['jquery']);
+    wp_enqueue_script('wp_auth_scripts');
+}
+
+add_action('wp_enqueue_scripts','wp_auth_load_assets');
+
+
+
+
+
+
