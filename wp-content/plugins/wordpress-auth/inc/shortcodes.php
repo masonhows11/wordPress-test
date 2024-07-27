4f2c2497c63f2f39dbc56b46555326c9f4fb09c2
mason.hows11 <mason.hows11@gmail.com>
2024-07-27 15:59:05 +0330

    show shortcodes in page temprory solved

--- wp-content/plugins/wordpress-auth/inc/shortcodes.php
+++ wp-content/plugins/wordpress-auth/inc/shortcodes.php
@@ -1,13 +1,16 @@
 <?php
-function wp_auth_login_handler($atts,$content = null)
+function wp_auth_login_handler($atts, $content = null)
 {
-    
-   include WP_AUTH_TPL . "front/login.php";
 
+    ob_start();
+    include WP_AUTH_TPL . "front/login.php";
+    return ob_get_clean();
 }
-function wp_auth_register_handler($atts,$content = null){
-
+function wp_auth_register_handler($atts, $content = null)
+{
+    ob_start();
     include WP_AUTH_TPL . "front/register.php";
+    return ob_get_clean();
 }
-add_shortcode('wp_auth_login','wp_auth_login_handler');
-add_shortcode('wp_auth_register','wp_auth_register_handler');
+add_shortcode('wp_auth_login', 'wp_auth_login_handler');
+add_shortcode('wp_auth_register', 'wp_auth_register_handler');
