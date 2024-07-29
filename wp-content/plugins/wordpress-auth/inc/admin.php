32010fed997db708b665c76696d7fb1fe2f70c8d
mason.hows11 <mason.hows11@gmail.com>
2024-07-29 22:29:01 +0330

    auth admin settings updated

--- wp-content/plugins/wordpress-auth/inc/admin.php
+++ wp-content/plugins/wordpress-auth/inc/admin.php
@@ -13,9 +13,22 @@ function wp_auth_admin_settings(){
 
 function wp_auth_settings(){
 
-    // save settings value
-    if(isset($_POST['submit'])){
 
+    $wp_auth_options = get_option('wp_auth_options',[]);
+
+    // save settings value
+    if(isset($_POST['saveData'])){
+
+        $wp_auth_options['is_login_active'] =
+            isset($_POST['is_login_active']);
+        $wp_auth_options['is_register_active'] =
+            isset($_POST['is_register_active']);
+        $wp_auth_options['login_title_form'] =
+            sanitize_text_field($_POST['login_title_form']);
+        $wp_auth_options['register_title_form'] =
+            sanitize_text_field($_POST['register_title_form']);
+
+        update_option('wp_auth_options',$wp_auth_options);
     }
 
      // show setting view
