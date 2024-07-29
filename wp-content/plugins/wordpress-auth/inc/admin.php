3cdff25a0327b26734a0b1b0c073af76b5d622ba
mason.hows11 <mason.hows11@gmail.com>
2024-07-30 00:21:05 +0330

    updated

--- wp-content/plugins/wordpress-auth/inc/admin.php
+++ wp-content/plugins/wordpress-auth/inc/admin.php
@@ -1,9 +1,6 @@
 <?php
-
-
 add_action('admin_menu','wp_auth_admin_settings');
 function wp_auth_admin_settings(){
-
     add_menu_page('ورود و ثبت نام',
         'ورود و ثبت نام',
         'manage_options',
@@ -12,13 +9,9 @@ function wp_auth_admin_settings(){
 }
 
 function wp_auth_settings(){
-
-
     $wp_auth_options = get_option('wp_auth_options',[]);
-
     // save settings value
     if(isset($_POST['saveData'])){
-
         $wp_auth_options['is_login_active'] =
             isset($_POST['is_login_active']);
         $wp_auth_options['is_register_active'] =
@@ -27,10 +20,8 @@ function wp_auth_settings(){
             sanitize_text_field($_POST['login_title_form']);
         $wp_auth_options['register_title_form'] =
             sanitize_text_field($_POST['register_title_form']);
-
         update_option('wp_auth_options',$wp_auth_options);
     }
-
      // show setting view
     include WP_AUTH_TPL . "admin/settings.php";
 }
