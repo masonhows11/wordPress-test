f3804d41704ba21df40c94462f619daeb85a2bad
mason.hows11 <mason.hows11@gmail.com>
2024-07-29 23:30:49 +0330

    use auth settings in front user updated

--- wp-content/plugins/wordpress-auth/inc/shortcodes.php
+++ wp-content/plugins/wordpress-auth/inc/shortcodes.php
@@ -3,6 +3,9 @@ function wp_auth_login_handler($atts, $content = null)
 {
 
     $wp_auth_options = get_option('wp_auth_options',[]);
+    if(isset($wp_auth_options['is_login_active']) && !$wp_auth_options['is_login_active']){
+        return '<div><p  style="text-align: center">امکان ورود در حال حاضز وجود ندارد</p></div>';
+    }
     ob_start();
     include WP_AUTH_TPL . "front/login.php";
     return ob_get_clean();
@@ -10,6 +13,9 @@ function wp_auth_login_handler($atts, $content = null)
 function wp_auth_register_handler($atts, $content = null)
 {
     $wp_auth_options = get_option('wp_auth_options',[]);
+    if(isset($wp_auth_options['is_register_active']) && !$wp_auth_options['is_register_active']){
+        return '<div><p  style="text-align: center">امکان ثبت نام در حال حاضز وجود ندارد</p></div>';
+    }
     ob_start();
     include WP_AUTH_TPL . "front/register.php";
     return ob_get_clean();
