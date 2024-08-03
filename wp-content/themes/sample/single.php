b6d2f62aa6f0146942b84bcf38eebfd36a1e466c
mason.hows11 <mason.hows11@gmail.com>
2024-08-03 17:12:50 +0330

    add content page

--- wp-content/themes/sample/single.php
+++ wp-content/themes/sample/single.php
@@ -1,1 +1,5 @@
-<?php
+<?php get_header() ?>
+<?php get_template_part('parts/top-navigation') ?>
+<?php get_template_part('parts/top-header') ?>
+<?php get_template_part('parts/content') ?>
+<?php get_footer() ?>
