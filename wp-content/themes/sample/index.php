40ab42d3d3134997cc72f312d3926efdbcc75532
mason.hows11 <mason.hows11@gmail.com>
2024-08-03 16:20:15 +0330

    add part files

--- wp-content/themes/sample/index.php
+++ wp-content/themes/sample/index.php
@@ -1,4 +1,4 @@
 <?php get_header() ?>
-<h1> hello this is sample wordPress  theme </h1>
-<h1> stop wordpress for now until </h1>
+<?php get_template_part('parts/top-navigation') ?>
+<?php get_template_part('parts/top-header') ?>
 <?php get_footer() ?>
