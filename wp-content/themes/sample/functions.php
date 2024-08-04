c01a819023c5d69f4f6e804ddd1175fe37b12ee8
mason.hows11 <mason.hows11@gmail.com>
2024-08-04 17:54:12 +0330

    functions.php updated

--- wp-content/themes/sample/functions.php
+++ wp-content/themes/sample/functions.php
@@ -1,4 +1,7 @@
 <?php
+// define const
+define('SAMPLE_PATH',get_template_directory());
+define('SAMPLE_URL',get_template_directory_uri());
 
 
 add_action('wp_enqueue_scripts', 'add_assets');
@@ -6,11 +9,14 @@ function add_assets()
 {
 
     // add styles
-    wp_register_style('main_style', get_template_directory_uri() . '/assets/css/main.css');
+    // wp_register_style('main_style', get_template_directory_uri() . '/assets/css/main.css');
+
+    wp_register_style('main_style', SAMPLE_URL . '/assets/css/main.css');
     wp_enqueue_style('main_style');
 
     // add scripts
-    wp_register_script('main_js', get_template_directory_uri() . '/assets/js/app.js', ['jquery'], false, true);
+    // wp_register_script('main_js', get_template_directory_uri() . '/assets/js/app.js', ['jquery'], false, true);
+    wp_register_script('main_js', SAMPLE_URL. '/assets/js/app.js', ['jquery'], false, true);
     wp_enqueue_script('main_js');
 
 }
@@ -30,3 +36,9 @@ function sample_setup_theme()
     add_theme_support('post-formats',array('gallery','video','audio'));
 
 }
+
+
+// for use custom functions file our theme
+// we can write any custom code in below file
+// and use this
+include SAMPLE_PATH."/includes/frontend/functions.php";
