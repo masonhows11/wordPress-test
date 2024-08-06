1669a32e75348d19096139302c0e202d3e5561c8
mason.hows11 <mason.hows11@gmail.com>
2024-08-06 17:24:53 +0330

    custom upload file done

--- wp-content/themes/sample/includes/admin/admin.php
+++ wp-content/themes/sample/includes/admin/admin.php
@@ -11,8 +11,29 @@ function sample_admin_menu(){
 
 }
 
+// for custom upload file
+// from parts/admin/menus.php page
 function sample_page_content(){
 
+    if(isset($_POST['upload'])){
+
+        // get file info
+        $file = $_FILES['uploadFile'];
+        // return file dir info
+        $uploadPath = wp_upload_dir();
+        // for save file to permanent dir
+        $file_path  = $uploadPath['basedir']."/my_upload_files\/";
+        // create dir
+        if(!file_exists($file_path)){
+            wp_mkdir_p($file_path);
+        }
+        // create new file name
+        $file_name_part = explode('.',$file['name']);
+        $new_file_name = 'file'.rand(10000,90000).'.'.end($file_name_part);
+        // final upload file
+        move_uploaded_file($file['tmp_name'],$file_path . $new_file_name);
+
+    }
     include SAMPLE_PATH."/parts/admin/menus.php";
 }
 function sample_add_media_uploader(){
