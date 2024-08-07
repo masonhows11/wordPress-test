d05f6488b42ec95dc9b3b534c03490e670191632
mason.hows11 <mason.hows11@gmail.com>
2024-08-07 17:04:58 +0330

    add functions for clab theme

--- wp-content/themes/clab/functions.php
+++ wp-content/themes/clab/functions.php
@@ -1,1 +1,17 @@
 <?php
+// define const
+define('CLAB_URL', get_template_directory_uri().'/');
+define('CLAB_PATH', get_template_directory().DIRECTORY_SEPARATOR);
+const CLAB_APP = CLAB_PATH . 'app' . DIRECTORY_SEPARATOR ;
+
+
+//
+add_action('after_theme_setup','clab_setup');
+function clab_setup(){
+
+}
+
+if(is_admin()){
+    include CLAB_APP . 'admin/admin.php';
+}
+include CLAB_APP . 'user/user.php';
