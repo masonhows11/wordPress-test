a41d51b635cd5f8b6ffd88d1a6b10cb2da179192
mason.hows11 <mason.hows11@gmail.com>
2024-08-07 17:30:13 +0330

    add assets file & config header footer

--- wp-content/themes/clab/functions.php
+++ wp-content/themes/clab/functions.php
@@ -4,12 +4,18 @@ define('CLAB_URL', get_template_directory_uri().'/');
 define('CLAB_PATH', get_template_directory().DIRECTORY_SEPARATOR);
 const CLAB_APP = CLAB_PATH . 'app' . DIRECTORY_SEPARATOR ;
 
+const CLAB_ASSETS_URL = CLAB_URL . 'assets';
+
 
 //
 add_action('after_theme_setup','clab_setup');
 function clab_setup(){
 
+    add_theme_support('title-tag');
 }
+
+// hide admin bar in front
+add_filter('show_admin_bar','__return_false');
 
 if(is_admin()){
     include CLAB_APP . 'admin/admin.php';
