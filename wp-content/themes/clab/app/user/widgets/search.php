15991c820b686bb0b4cb13aa22592f31d50730b7
mason.hows11 <mason.hows11@gmail.com>
2024-08-08 16:05:32 +0330

    add serch widget updated

---
+++ wp-content/themes/clab/app/user/widgets/search.php
@@ -0,0 +1,46 @@
+<?php
+
+class Clab_search_widget extends WP_Widget
+{
+
+    public function __construct($id_base, $name)
+    {
+        parent::__construct(false, 'ابزارک جستجو قالب');
+    }
+
+
+    public function widget($args, $instance)
+    {
+        ?>
+
+
+
+        <?php
+    }
+
+    public function form($instance)
+    {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : 'جستجو';
+        ?>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">عنوان :</label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+        </p>
+        <?php
+    }
+
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+        return $instance;
+    }
+
+}
+
+function register_clab_search_widget()
+{
+    register_widget('Clab_search_widget');
+}
+
+add_action('widget_init', 'register_clab_search_widget');
