83939036e4832ca34fa9d44e7b593697c63adbc6
mason.hows11 <mason.hows11@gmail.com>
2024-08-08 15:28:56 +0330

    add serch updated

---
+++ wp-content/themes/clab/tpls/blog/search/header.php
@@ -0,0 +1,21 @@
+<!--page title start-->
+<section class="section-gap section-top pb-0 bg-white text-center">
+    <!--<div class="hero-img bg-overlay" data-overlay="1" style="background-image: url(assets/img/price-banner.jpg);"></div>-->
+    <div class="container">
+        <div class="row justify-content-md-center align-items-center">
+            <div class="col-md-7">
+                <!-- heading -->
+                <h2 class="">
+                    تمام نوشته های مربوط با عبارت :
+                    <br>
+                    <?php echo get_search_query(); ?>
+                </h2>
+            </div>
+            <div class="col-md-8">
+                <img src="<?php echo CLAB_URL; ?>assets/img/blog/blog-banner.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<!--page title end-->
+
