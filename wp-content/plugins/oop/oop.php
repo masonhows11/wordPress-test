4c477ff1c8321a00056b67d6de7182658addec5e
mason.hows11 <mason.hows11@gmail.com>
2024-08-11 23:04:01 +0330

    add plugin using oop

---
+++ wp-content/plugins/oop/oop.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: oop plugin test
+Plugin URI: http://websolor.ir/
+Description: یک افزونه شی گرایی وردپرس
+Author: Naeem soltany
+Text Domain: oop test
+Version: 1.0.0
+Author URI: http://websolor.ir/
+*/
