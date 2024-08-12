70b6d69ff724b4a436d078f38947839910e52d36
mason.hows11 <mason.hows11@gmail.com>
2024-08-12 17:53:23 +0330

    ooop plugin updated

--- wp-content/plugins/oop-plugin/oop-plugin.php
+++ wp-content/plugins/oop-plugin/oop-plugin.php
@@ -10,3 +10,50 @@ Version: 1.0.0
 Author URI: http://websolor.ir/
 */
 
+class OOP_Plugin{
+
+    public function __construct()
+    {
+      
+        $this->Constants();
+    }
+
+
+    public function Constants()
+    {
+        
+        // get dir path
+        define('OOP_DIR',plugin_dir_path(__FILE__));
+        // get url path
+        define('OOP_URL',plugin_dir_url(__FILE__));
+
+    }
+
+
+    public function init()
+    {
+    
+        register_activation_hook(__FILE__,[$this,'activation']);
+        register_deactivation_hook(__FILE__, [$this,'deactivation']);
+    }
+
+    public function activation()
+    {
+        
+    }
+
+    public function deactivation()
+    {
+        
+    }
+
+    public function register_assets()
+    {
+        
+        
+
+    }
+
+
+
+}
