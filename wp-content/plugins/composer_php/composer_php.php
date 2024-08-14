813f9cd3cb87e81f2c5b4d41dfce4b8cd2e0b8bd
mason.hows11 <mason.hows11@gmail.com>
2024-08-14 22:08:28 +0330

    use composer in wp updated

---
+++ wp-content/plugins/composer_php/composer_php.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: composer in wordpress
+Plugin URI: http://websolor.ir/
+Description: استفاده از کامپوزر در وردپرس
+Author: Naeem soltany
+Text Domain: composer in wp
+Version: 1.0.0
+Author URI: http://websolor.ir/
+*/
