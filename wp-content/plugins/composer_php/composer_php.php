18056f01ef9c0658dc0d1f671772b5590613e2e0
mason.hows11 <mason.hows11@gmail.com>
2024-08-14 22:35:51 +0330

    use composer in wp add autoload updated

--- wp-content/plugins/composer_php/composer_php.php
+++ wp-content/plugins/composer_php/composer_php.php
@@ -8,3 +8,8 @@ Text Domain: composer in wp
 Version: 1.0.0
 Author URI: http://websolor.ir/
 */
+
+// for load autoload class
+// load required class
+define('PHP_COMPOSER_ROOT',plugin_dir_path(__FILE__));
+require PHP_COMPOSER_ROOT . 'vendor/autoload.php';
